b8c9c3031fa78ece48dcb0670f9221a67bdc99b3
zahra-karami <zahra.karami@live.com>
2020-01-12 02:33:55 -0500

    zig-zag algorithm
    Convert array into Zig-Zag fashion

---
+++ ZigZag.cs
@@ -0,0 +1,50 @@
+﻿namespace Algorithms
+{
+    public class ZigZag
+    {
+        /*
+        Convert array into Zig-Zag fashion
+        Given an array of DISTINCT elements, rearrange the elements of array in zig-zag fashion in O(n) time. 
+        The converted array should be in form a < b > c < d > e < f.
+        Example:
+        Input: arr[] = {4, 3, 7, 8, 6, 2, 1}
+        Output: arr[] = {3, 7, 4, 8, 2, 6, 1}
+        */
+
+        private readonly int[] _arr;
+        public ZigZag(int[] arr)
+        {
+            _arr = arr;
+        }
+        public int[] Do()
+        {
+            var n = _arr.Length - 1;
+            var flag = true;
+            var i = 0;
+
+            while (i < n)
+            {
+                if (!flag && _arr[i] < _arr[i + 1])
+                {
+                    Swap(i);
+                }
+                else if (flag && _arr[i] > _arr[i + 1])
+                {
+                    Swap(i);
+                }
+                i++;
+                flag = !flag;
+            }
+
+            return _arr;
+
+        }
+
+        private void Swap(int i)
+        {
+            var temp = _arr[i];
+            _arr[i] = _arr[i + 1];
+            _arr[i + 1] = temp;
+        }
+    }
+}
